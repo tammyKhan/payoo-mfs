fdecaa08781bef57263f27057ab659994a45e06b
tammykhan <tammykhan543@gmail.com>
2024-09-19 12:06:26 +0600

    added home page

--- js/login.js
+++ js/login.js
@@ -2,10 +2,21 @@
 document.getElementById('button-login').addEventListener('click', function(event){
   //  step - 2 prevent default behavior (prevent reloading browser)
   event.preventDefault();
-  console.log('login button clicked');
 
 //  step - 3 get the phone number
   const phoneNumber = document.getElementById('phone-number').value;
-  console.log(phoneNumber)
+  const pinNumber = document.getElementById('pin-number').value;
+
+  console.log(phoneNumber);
+  console.log(pinNumber);
+
+  // step - 4 validate phone and pin
+  if(phoneNumber === '5' && pinNumber === '1234'){
+    console.log('you are logged in');
+    window.location.href = '/home.html';
+    // step-5: allow user to use the website
+  }else{
+    alert('Wrong phone Number or Pin');
+  }
 
 })
