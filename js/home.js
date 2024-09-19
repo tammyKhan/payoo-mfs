247daf7ae8f23b4dd4de3f75e87e52bd367ee3ed
tammykhan <tammykhan543@gmail.com>
2024-09-19 13:48:14 +0600

    added partially the add money event handler andupdate the balance ini the UI/DOM

--- js/home.js
+++ js/home.js
@@ -10,4 +10,27 @@ console.log(addMoneyInput);
 const pinNumberInput = document.getElementById('input-pin-number').value;
 console.log(pinNumberInput);
 
+// step-3: verify pin number
+if(pinNumberInput === '1234'){ 
+  console.log('adding money to your account');
+
+// step-4: get the current balance
+const balance = document.getElementById('account-balance').innerText;
+console.log(balance)
+
+// step-5: add addMoneyInput with balance
+const addMoneyNumber = parseFloat(addMoneyInput);
+const balanceNumber = parseFloat(balance);
+const newBalance = addMoneyNumber + balanceNumber;
+
+console.log(newBalance);
+
+// step-6: update the balance ini the UI/DOM
+document.getElementById('account-balance').innerText = newBalance;
+
+
+}else{
+  alert('please try again later')
+}
+
 })
